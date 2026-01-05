cbf47de632d16e29b42133079fe9e66322364376
Ashish Mondal <mimokool2011@gmail.com>
2026-01-05 18:30:22 +0530

    Verify Github webhook push trigger

--- lab-23-jenkins-docker-build/app/index.js
+++ lab-23-jenkins-docker-build/app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Github webhook triggered Jenkins CI/CD🚀");
+  res.end("Webhook pushed verified🚀");
 }).listen(3000);
 
 console.log("Server running on port 3000");
